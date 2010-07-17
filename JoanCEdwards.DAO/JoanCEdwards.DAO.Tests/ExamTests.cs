33d2109e981b58637eced641a572103f91c4b33c
Patrick Sullivan <sully317@gmail.com>
2010-07-17 14:14:58 -0400

    added Exam tests

---
+++ JoanCEdwards.DAO/JoanCEdwards.DAO.Tests/ExamTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace JoanCEdwards.DAO.Tests
+{
+    [TestFixture]
+    public class ExamTests
+    {
+        [Test]
+        public void Exam_WhenCreated_HasAnID()
+        {
+            var db = new ExamSystemDataContext();
+            var exam = new Exam() { Instructions = new string('a', 3500), Title = "here is the title" };
+            db.Exams.InsertOnSubmit(exam);
+            db.SubmitChanges();
+            var actualExamId = db.Exams.First<Exam>().ExamId;
+            Assert.AreEqual(1, actualExamId);
+        }
+
+        [Test]
+        public void Exam_WhenDeleted_StatusIsFalse()
+        {
+            ExamSystemDataContext db = new ExamSystemDataContext();
+            Exam exam = new Exam() { Instructions = new string('a', 3500), Title = "here is the title" };
+            db.Exams.InsertOnSubmit(exam);
+            db.SubmitChanges();
+            db.DeleteExam(exam.ExamId);
+            Assert.AreEqual(false, exam.Status);
+        }
+    }
+}
