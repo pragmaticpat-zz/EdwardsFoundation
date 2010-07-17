8d66849c7a7f339ec785b934a5ac0cfc59c872e0
Patrick Sullivan <sully317@gmail.com>
2010-07-17 12:00:44 -0400

    initial commit

---
+++ JoanCEdwards.DAO/JoanCEdwards.DAO.Tests/UserProfileTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace JoanCEdwards.DAO.Tests
+{
+    [TestFixture]
+    public class UserProfileTests
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void UserProfile_WhenStored_ReturnsId()
+        {
+            ExamSystemDataContext db = new ExamSystemDataContext();
+            UserProfile profile = new UserProfile(){EmailAddress = "email", FirstName = "name", LastName = "lname", GradeLevel = "5", UserType = 'S'};
+            db.UserProfiles.InsertOnSubmit(profile);
+            db.SubmitChanges();
+            Assert.AreEqual(1, db.UserProfiles.First().UserId);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            ExamSystemDataContext db = new ExamSystemDataContext();
+            db.ExecuteCommand("truncate table dbo.UserProfile");
+            db.SubmitChanges();
+        }
+
+    }
+}
