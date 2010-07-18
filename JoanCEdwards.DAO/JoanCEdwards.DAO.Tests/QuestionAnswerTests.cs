2357a0a28d62d4a239481bd47f4247e22a6ef4c2
Patrick Sullivan <sully317@gmail.com>
2010-07-18 03:23:34 -0400

    more cleanup

--- JoanCEdwards.DAO/JoanCEdwards.DAO.Tests/QuestionAnswerTests.cs
+++ JoanCEdwards.DAO/JoanCEdwards.DAO.Tests/QuestionAnswerTests.cs
@@ -14,13 +14,13 @@ namespace JoanCEdwards.DAO.Tests
         public void Question_HasOneAnswer_PerUser()
         {
             var exam = new Exam() { Instructions = new string('a', 3500), Title = "here is the title" };
-            var question = new Question() { QuestionCategory = "label", QuestionText = "here is the question text", QuestionType = "M" };
             db.Exams.InsertOnSubmit(exam);
+            var question = new Question() { QuestionCategory = "label", QuestionText = "here is the question text", QuestionType = "M" };
             db.Questions.InsertOnSubmit(question);
             var expectedProfile = new UserProfile() { EmailAddress = "email", FirstName = "name", LastName = "lname", GradeLevel = "5", UserType = 'S', Password = new string('p', 300) };
             db.UserProfiles.InsertOnSubmit(expectedProfile);
-            exam.ExamQuestions.Add(new ExamQuestion() { Exam = exam, Question = question, SortOrder = 1 });
             question.QuestionAnswers.Add(new QuestionAnswer() { Question = question, Exam = exam, UserProfile = expectedProfile, Answer = "answer" });
+            exam.ExamQuestions.Add(new ExamQuestion() { Exam = exam, Question = question, SortOrder = 1 });
             db.SubmitChanges();
 
             var actualQuestion = (from e in db.Questions
