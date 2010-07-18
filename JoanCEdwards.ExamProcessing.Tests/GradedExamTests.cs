c1cb6e2cd9b139e901a3e0b6194a19257ce61567
Patrick Sullivan <sully317@gmail.com>
2010-07-18 07:46:06 -0400

    started building tests for the workflows, updated diagram layout

---
+++ JoanCEdwards.ExamProcessing.Tests/GradedExamTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace JoanCEdwards.ExamProcessing.Tests
+{
+    [TestFixture]
+    public class GradedExamTests
+    {
+        [Test]
+        public void Exam_WhenCompletedByStudent_IsAssignedToMentor()
+        {
+            Assert.Fail("not yet implemented");
+        }
+
+        [Test]
+        public void Exam_WhenCompletedByMentor_IsAssignedToDirector()
+        {
+            Assert.Fail("not yet implemented");
+        }
+
+        [Test]
+        public void Exam_WhenCompletedByDirector_HasTotalScore()
+        {
+            Assert.Fail("not yet implemented");
+        }
+    }
+}
