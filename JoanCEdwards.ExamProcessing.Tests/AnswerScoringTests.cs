d80966f82beebfc861566cb5d586a9354f300a5b
Patrick Sullivan <sully317@gmail.com>
2010-07-18 10:36:53 -0400

    started adding tests for scoring answers

---
+++ JoanCEdwards.ExamProcessing.Tests/AnswerScoringTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace JoanCEdwards.ExamProcessing.Tests
+{
+    [TestFixture]
+    public class AnswerScoringTests
+    {
+        [Test]
+        public void Question_AnsweredCorrectly_GetsAllPoints()
+        {
+        }
+    }
+}
